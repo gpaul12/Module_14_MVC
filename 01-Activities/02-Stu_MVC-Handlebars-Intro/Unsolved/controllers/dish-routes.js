ce10e99a2bfb53f8ee816abd4d642730c0d41d76
Gerry Paul <gpaul12@nc.rr.com>
2023-04-20 21:28:35 -0400

    Day 1

--- 01-Activities/02-Stu_MVC-Handlebars-Intro/Unsolved/controllers/dish-routes.js
+++ 01-Activities/02-Stu_MVC-Handlebars-Intro/Unsolved/controllers/dish-routes.js
@@ -1,11 +1,14 @@
 // TODO: Add a comment indicating how this file fits into the MVC framework (is it a Model, a View, or a Controller?) and what it is responsible for handling.
+// It is a controller.  Handles routes and routing.
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 // TODO: Add a comment describing the purpose of the get route
-router.get('/', async (req, res) => {
-//TODO: Add a comment describing the purpose of the render method
-  res.render('all');
+// Default route to home page
+router.get("/", async (req, res) => {
+  //TODO: Add a comment describing the purpose of the render method
+  //
+  res.render("all");
 });
 
 module.exports = router;
--- 01-Activities/10-Stu_Handlebars-FE-Logic/Unsolved/controllers/api/dish-routes.js
+++ 01-Activities/10-Stu_Handlebars-FE-Logic/Unsolved/controllers/api/dish-routes.js
@@ -1,42 +1,46 @@
-const router = require('express').Router();
-const Dish = require('../../models/Dish');
+const router = require("express").Router();
+const Dish = require("../../models/Dish");
 
 // route to create/add a dish
-router.post('/', async (req, res) => {
-  try { 
-    const dishData = await Dish.create({
-    dish_name: req.body.dish_name,
-    description: req.body.description,
-    guest_name: req.body.guest_name,
-    has_nuts: req.body.has_nuts,
-  });
-  res.status(200).json(dishData)
-} catch (err) {
-  res.status(400).json(err);
-}
-});
-
-// TODO: According to MVC, what is the role of this action method?
-router.put('/:id', async (req, res) => {
-  // TODO: Where is this action method sending the data from the body of the fetch request? Why?
+router.post("/", async (req, res) => {
   try {
-    const dish = await Dish.update(
-    {
+    const dishData = await Dish.create({
       dish_name: req.body.dish_name,
       description: req.body.description,
       guest_name: req.body.guest_name,
       has_nuts: req.body.has_nuts,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
     });
+    res.status(200).json(dishData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// TODO: According to MVC, what is the role of this action method?
+// the is the constroller. it accepts input and sends data to the Modeul and the View
+router.put("/:id", async (req, res) => {
+  // TODO: Where is this action method sending the data from the body of the fetch request? Why?
+  // sending the data to the Model so that one dish can be updated
+  try {
+    const dish = await Dish.update(
+      {
+        dish_name: req.body.dish_name,
+        description: req.body.description,
+        guest_name: req.body.guest_name,
+        has_nuts: req.body.has_nuts,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
     // TODO: If the database is updated successfully, what happens to the updated data below?
+    // The JSON file is updated with updated info
     res.status(200).json(dish);
   } catch (err) {
-      res.status(500).json(err);
-    };
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
